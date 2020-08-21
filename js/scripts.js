a49520c08457677df9e243ea22b622d001f5e59c
Mark Tiapkov <mark.tiapkov@gmail.com>
2020-08-21 13:27:13 -0700

    Add additional string variable

--- js/scripts.js
+++ js/scripts.js
@@ -12,13 +12,14 @@ $(document).ready(function(){
 function rogSays(number) {
   const numArray = []
   for (let i = 0; i <= number; i++) {
-    if([i].includes(3) || i === 13 || i >= 30 && i < 40 || i === 43|| i === 53) {
+    let emptyString = i + "";
+    if (emptyString.includes(3)) {
       numArray.push(" Won't you be my neighbor?")
     }
-    else if ([i].includes (2) || i >= 20 && i < 30 || i === 42 || i === 52) {
+    else if (emptyString.includes (2)) {
       numArray.push(" Boop!")
     }
-    else if ([i].includes (1) || i >= 10 && i < 20 || i === 41 || i == 51) {
+    else if (emptyString.includes (1)) {
       numArray.push(" Beep!")
     }
     else {
