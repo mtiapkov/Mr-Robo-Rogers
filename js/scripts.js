bc81cdf7840a859134dca7513eeb678a3269365a
Mark Tiapkov <mark.tiapkov@gmail.com>
2020-08-21 10:01:34 -0700

    Add a function, if statement to check input

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,27 @@
 $(document).ready(function(){
   $("form#rodgers").submit(function(){
     event.preventDefault();
-    const number = parseInt($("#number").val())
-    const output = 
+    const number = $("#number").val()
+    const output = rogSays(number)
     $("output").text(output);
   })
 })
+
+//Business Logic
+function rogSays (number) {
+  const numArray =[]
+  for (i = 0; i <= number; i++) {
+    if(i.includes(3)) {
+      numArray.push("won't you be my neighbor?")
+    }
+    else if (i === 2) {
+      numArray.push("Boop!")
+    }
+    else if (i === 1) {
+      numArray.push("Beep!")
+    }
+    else {
+      numArray.push(" " + i);}
+  }
+  return result = numArray
+}
