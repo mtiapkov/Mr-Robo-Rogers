c6489a425072c74dfb31f75a5bc38f61361a8c30
Mark Tiapkov <mark.tiapkov@gmail.com>
2020-08-21 11:05:01 -0700

    Adjust if statements

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
     event.preventDefault();
     const number = $("#number").val()
     const output = rogSays(number)
-    $("output").text(output);
+    $("#output").text(output);
   })
 })
 
@@ -12,17 +12,18 @@ $(document).ready(function(){
 function rogSays(number) {
   const numArray = []
   for (let i = 0; i <= number; i++) {
-    if([i].includes(3) || i === 13 || i >= 30 && i < 40) {
+    if([i].includes(3) || i === 13 || i >= 30 && i < 40 || i === 43|| i === 53) {
       numArray.push(" Won't you be my neighbor?")
     }
-    else if ([i].includes (2) || i >= 20 && i < 30) {
+    else if ([i].includes (2) || i >= 20 && i < 30 || i === 42 || i === 52) {
       numArray.push(" Boop!")
     }
-    else if ([i].includes (1) || i >= 10 && i < 20) {
+    else if ([i].includes (1) || i >= 10 && i < 20 || i === 41 || i == 51) {
       numArray.push(" Beep!")
     }
     else {
-      numArray.push(" " + i);}
+      numArray.push(" " + i);
+    }
   }
-  return result = numArray
+  return output = numArray
 }
